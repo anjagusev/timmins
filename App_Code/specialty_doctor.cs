de7781b6b475e92dfd41c2a945bb80180103b501
Harinder Singh <hharrysidhu@gmail.com>
2013-03-24 15:49:45 -0700

    FindaDoctor.aspx is added linq class file is added

---
+++ App_Code/specialty_doctor.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for specialty_doctor
+/// </summary>
+public class specialty_doctor
+{
+   // Get all doctors from table
+    public IQueryable<tbl_doctor> getdoctors()
+    {
+        specialties_doctorsDataContext objdocDC = new specialties_doctorsDataContext();
+        //creating an anonymous variable with its value being the instance of our LINQ object
+        var alldoctors = objdocDC.tbl_doctors.Select(x => x);// method syntax //products is the name of the actual table. //==> means goes to, x means column name
+        //var allProducts = from x in objProdDC.products select x; //query syntax
+        return alldoctors;
+    }
+
+    // get all specialities
+    public IQueryable<tbl_specialty> getspecialities()
+    {
+        specialties_doctorsDataContext objspDC = new specialties_doctorsDataContext();
+        //creating an anonymous variable with its value being the instance of our LINQ object
+        var allspecialities = objspDC.tbl_specialties.Select(x => x);// method syntax //products is the name of the actual table. //==> means goes to, x means column name
+        //var allProducts = from x in objProdDC.products select x; //query syntax
+        return allspecialities;
+    }
+
+
+    // get doctors by ID
+    public IQueryable<tbl_doctor> getdoctorByID(int _id)
+    {
+        specialties_doctorsDataContext objdocDC = new specialties_doctorsDataContext();
+        var doctorID = objdocDC.tbl_doctors.Where(x => x.doctor_id == _id).Select(x => x);
+        return doctorID;
+    }
+
+    // get specialty by ID
+    public IQueryable<tbl_specialty> getspecialtyByID(int _id)
+    {
+        specialties_doctorsDataContext objspDC = new specialties_doctorsDataContext();
+        var specialtyID = objspDC.tbl_specialties.Where(x => x.specialty_id == _id).Select(x => x);
+        return specialtyID;
+    }
+
+
+    // specialty dropdown selecting doctors by specialty id
+    public IQueryable<tbl_doctor> getdoctorbySpid( int _id)
+    {
+        specialties_doctorsDataContext objdocDC = new specialties_doctorsDataContext();
+        var ret_doc = objdocDC.tbl_doctors.Where(x => x.specialty_id == _id).Select(x => x);
+        return ret_doc;
+    }
+
+    public string ret_doc;
+
+    public int docCount()
+    {
+        specialties_doctorsDataContext objdocDC = new specialties_doctorsDataContext();
+        var found = ret_doc.Count();
+        return found;
+    }
+
+
+    //search by name
+    public IQueryable<tbl_doctor> docByName(string _fname, string _lname )
+    {
+        specialties_doctorsDataContext objdocDC = new specialties_doctorsDataContext();
+        var names = objdocDC.tbl_doctors.Where(x =>x.first_name.Contains(_fname)).Where (x =>x.last_name.Contains(_lname));
+        return names;
+    
+    }
+
+
+
+
+    // insert part for doctors table
+
+    public bool commitInsertInDoc(int _specialtyID, string _firstname, string _lastname, string _gender, string _email, string _bio)
+    {
+        specialties_doctorsDataContext objdocDC = new specialties_doctorsDataContext();
+        using (objdocDC)
+        {
+            tbl_doctor objNewdoc = new tbl_doctor();
+            objNewdoc.first_name = _firstname;
+            objNewdoc.last_name = _lastname;
+            objNewdoc.specialty_id = _specialtyID;
+            objNewdoc.bio = _bio;
+            objNewdoc.email = _email;
+            objNewdoc.gender = _gender;
+            //insert command
+            objdocDC.tbl_doctors.InsertOnSubmit(objNewdoc);
+            //commit insert against database
+            objdocDC.SubmitChanges();
+            return true;
+        }
+    }
+    //insert for specialty
+
+    public bool commitInsertInSp( string _specialty)
+    {
+        specialties_doctorsDataContext objspDC = new specialties_doctorsDataContext();
+        using (objspDC)
+        {
+            tbl_specialty objNewsp = new tbl_specialty();
+           
+            objNewsp.specialty = _specialty;
+            objspDC.tbl_specialties.InsertOnSubmit(objNewsp);
+            objspDC.SubmitChanges();
+            return true;
+        }
+    }
+
+    // doctors insert
+    public bool commitUpdateInDoc(int _specialtyID, string _firstname, string _lastname, string _gender, string _email, string _bio)
+    {
+        specialties_doctorsDataContext objdocDC = new specialties_doctorsDataContext();
+        using (objdocDC)
+        {
+            var objUpdoc = objdocDC.tbl_doctors.Single(x => x.specialty_id == _specialtyID);
+            objUpdoc.first_name = _firstname;
+            objUpdoc.last_name = _lastname;
+            objUpdoc.bio = _bio;
+            objUpdoc.email = _email;
+            objUpdoc.gender = _gender;
+            objdocDC.SubmitChanges();
+            return true;
+        }
+    }
+
+// update for specialty table
+
+    public bool commitUpdateInSp(int _specialtyID, string _specialty)
+    {
+        specialties_doctorsDataContext objspDC = new specialties_doctorsDataContext();
+        using (objspDC)
+        {
+            var objUpdoc = objspDC.tbl_specialties.Single(x => x.specialty_id == _specialtyID);
+            objUpdoc.specialty = _specialty;
+            objspDC.SubmitChanges();
+            return true;
+        }
+    }
+
+
+    // delete for specialty and doctors
+
+    public bool commitDeleteInDoc(int _doctorID)
+    {
+        specialties_doctorsDataContext objdocDC = new specialties_doctorsDataContext();
+        using (objdocDC)
+        {
+            var objDeldoc = objdocDC.tbl_doctors.Single(x => x.doctor_id == _doctorID);
+            objdocDC.tbl_doctors.DeleteOnSubmit(objDeldoc);
+            objdocDC.SubmitChanges();
+            return true;
+        }
+    }
+
+
+
+
+    //public bool commitDeleteInSp(int _specialtyID)
+    //{
+    //    specialties_doctorsDataContext objspDC = new specialties_doctorsDataContext();
+    //    using (objspDC)
+    //    {
+    //        var objDelsp = objspDC.tbl_doctors.Single(x => x.specialty_id == _specialtyID);
+    //        objspDC.tbl_specialties.DeleteOnSubmit(objDelsp);
+    //        objspDC.SubmitChanges();
+    //        return true;
+    //    }
+    //}
+
+    //public bool commitInsertInSp(string p1, string p2)
+    //{
+    //    throw new NotImplementedException();
+    //}
+}
---
+++ FindaDoctor.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="FindaDoctor.aspx.cs" Inherits="FindaDoctor" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+     <asp:label ID="lbl_Sspecialty" runat="server" Text="Search by specialty:" />
+        <br />
+        <asp:Label ID="lbl_speciality" runat="server" Text="Select Specialty: " />
+        <asp:DropDownList ID="ddl_sp" runat="server" AutoPostBack="true" OnSelectedIndexChanged="subChange" >
+        <asp:ListItem Text="select Speciality" Value="0" />    
+        </asp:DropDownList>
+        <br />
+        <br />
+        <asp:label ID="lbl_Sname" runat="server" Text="Search by Name" />
+        <br />
+        <asp:Label ID="lbl_fname" runat="server" Text="Enter First Name: " AssociatedControlID="txt_fname" />
+        <asp:TextBox ID="txt_fname" runat="server" />
+        <br />
+        <asp:Label ID="lbl_lname" runat="server" Text="Enter Last Name: " AssociatedControlID="txt_lname" />
+        <asp:TextBox ID="txt_lname" runat="server" />
+        <br />
+        <asp:Button ID="btn" runat="server" Text="Search"  OnClick="search"/>
+       
+      <br />
+        <asp:Label ID="no_result" runat="server" />
+        <br />
+
+      <%--  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++--%>
+
+         <asp:GridView ID="grd_doc" Runat="server" 
+          AutoGenerateColumns="False"
+            AllowSorting="True" BorderWidth="2px" BackColor="White" 
+            GridLines="None" CellPadding="3"
+            CellSpacing="1" BorderStyle="Ridge" BorderColor="White"
+            AllowPaging="True">
+            <FooterStyle ForeColor="Black" BackColor="#C6C3C6"></FooterStyle>
+            <PagerStyle ForeColor="Black" HorizontalAlign="Right"  BackColor="#C6C3C6"></PagerStyle>
+            <HeaderStyle ForeColor="#E7E7FF" Font-Bold="True" BackColor="#4A3C8C"></HeaderStyle>
+            <Columns>
+                <asp:BoundField HeaderText="First Name"  DataField="first_name" SortExpression="Name"/>
+                
+                <asp:BoundField HeaderText="Last_Name"  DataField="last_name" SortExpression="UnitPrice">
+                    <ItemStyle HorizontalAlign="Right"></ItemStyle>
+                </asp:BoundField>
+                <asp:BoundField HeaderText="Gender"  DataField="gender" DataFormatString="{0:d}">
+                    <ItemStyle HorizontalAlign="Right"></ItemStyle>
+                </asp:BoundField>
+               <asp:hyperlinkField DataTextField="email" 
+                         HeaderText="email" 
+                        SortExpression="ProductName" 
+                        DataNavigateUrlFields="doctor_id" 
+                         DataNavigateUrlFormatString="" />
+                <asp:BoundField HeaderText="bio" DataField="bio"></asp:BoundField>
+            </Columns>
+            <SelectedRowStyle ForeColor="White" Font-Bold="True" 
+                 BackColor="#9471DE"></SelectedRowStyle>
+            <RowStyle ForeColor="Black" BackColor="#DEDFDE"></RowStyle>
+        </asp:GridView>
+        <i>You are viewing page
+        <%=grd_doc.PageIndex + 1%>
+        of
+        <%=grd_doc.PageCount%>
+        </i>
+    </div>
+    </form>
+</body>
+</html>

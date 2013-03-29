808b7b1e3770c613751524df68a9950833dd65fe
Jymele <jymele@yahoo.fr>
2013-03-29 02:53:29 -0400

    added a few things for the news feed

--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -56,6 +56,9 @@ public partial class MasterPage : System.Web.UI.MasterPage
             case "Showall":
                 _showNews(int.Parse(e.CommandArgument.ToString()));
                 break;
+            case "Feed":
+                _subRebind();
+                break;
         }
     }
 }

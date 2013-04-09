a5fb6f6acb18cb47f87876581295ed7a3428e4c8
Anja <anja.gusev@gmail.com>
2013-04-09 14:12:54 -0400

    Managing Survey Responses

---
+++ Admin/SurveyResults.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/adminSubPage.master" AutoEventWireup="true" CodeFile="SurveyResults.aspx.cs" Inherits="Admin_Default2" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cph_admin_main" Runat="Server">
+ <div>
+        <table cellpadding="3" cellspacing="5" style="border: 1px solid black; border-collapse: collapse;">
+            <thead>
+                <tr>
+                    <th style="width: 120px;">
+                       Survey Response ID
+                          <asp:LinkButton ID="lkb_srDown" runat="server" OnClick="srDown_Click">Down</asp:LinkButton>
+                        <asp:LinkButton Visible="false" ID="lkb_srUp" runat="server" OnClick="srUp_Click">UP</asp:LinkButton>
+                    </th>
+                    <th>
+                        Survey Title &nbsp;
+                        <asp:LinkButton ID="lkb_titleDown" runat="server" OnClick="titleDown_Click">Down</asp:LinkButton>
+                        <asp:LinkButton Visible="false" ID="lkb_titleUp" runat="server" OnClick="titleUp_Click">UP</asp:LinkButton>
+                    </th>
+                    <th>
+                        SurveyQuestion
+                           <asp:LinkButton ID="lkb_questionDown" runat="server" OnClick="questionDown_Click">Down</asp:LinkButton>
+                        <asp:LinkButton Visible="false" ID="lkb_questionUp" runat="server" OnClick="questionUp_Click">UP</asp:LinkButton>
+                    </th>
+                    <th>
+                        Response
+                    </th>
+                </tr>
+            </thead>
+            <asp:Repeater ID="ltv_results" runat="server" OnItemDataBound="subBound">
+                <ItemTemplate>
+                    <table cellpadding="3" cellspacing="5" style="border: 1px solid black; border-collapse: collapse;">
+                        <tr>
+                            <td style="width: 120px">
+                                <asp:HiddenField ID="hdfID" runat="server" Value='<%#Eval("ID") %>' />
+                                <asp:Label ID="lbl_surveyresponseid" runat="server" Text='<%#Eval("ID") %>' />
+                            </td>
+                            <td>
+                                <asp:Repeater ID="rpt_surveytitle" runat="server">
+                                    <ItemTemplate>
+                                        <asp:Label ID="lblTitle" runat="server" Text='<%#Eval("Title")%>' />
+                                    </ItemTemplate>
+                                </asp:Repeater>
+                                <asp:HiddenField ID="hdf_SID" runat="server" Value='<%#Eval("SurveyID") %>' />
+                            </td>
+                            <td>
+                                <asp:Repeater ID="rpt_question" runat="server">
+                                    <ItemTemplate>
+                                        <asp:Label ID="lblQuestion" runat="server" Text='<%#Eval("Text")%>' />
+                                    </ItemTemplate>
+                                </asp:Repeater>
+                                <asp:HiddenField ID="hdf_QID" runat="server" Value='<%#Eval("QuestionID") %>' />
+                            </td>
+                            <td>
+                                <asp:Label ID="lbl_value" runat="server" Text='<%#Eval("Value") %>' />
+                            </td>
+                        </tr>
+                    </table>
+                </ItemTemplate>
+            </asp:Repeater>
+            <asp:Literal ID="lbl_surveyid" runat="server" />
+    </div>
+</asp:Content>
+
---
+++ Admin/SurveyResults.aspx.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Admin_Default2 : System.Web.UI.Page
+{
+
+
+    DataClassesDataContext dbDC = new DataClassesDataContext();
+    // SurveyResponseDataContext SRDC = new SurveyResponseDataContext();
+    SurveyResponseClass objSurveyResponse = new SurveyResponseClass();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        objSurveyResponse.getSurveyResponses();
+
+        if (!Page.IsPostBack)
+        {
+            ltv_results.DataSource = objSurveyResponse.getSurveyResponses();
+            ltv_results.DataBind();
+        };
+
+        //Literal lbl_surveyid = (Literal)FindControl("lbl_surveyid");
+
+        HiddenField hdf = (HiddenField)FindControl("hdfID");
+        // int surveyresponseid = Convert.ToInt32(hdf.Value.ToString());
+        //HiddenField hdfSID = (HiddenField)FindControl("hdf_SID");
+        //int surveyID = Convert.ToInt32(hdfSID);
+
+        //List<string> surveytitle = (from q in dbDC.Questions
+        //                            join sq in dbDC.SurveyQuestions on q.ID equals sq.QuestionID
+        //                            join sr in dbDC.SurveyResponses on sq.SurveyID equals sr.SurveyID
+        //                            join s in dbDC.Surveys on sr.SurveyID equals s.ID
+        //                            where s.ID == surveyID
+        //                            select s.Title).ToList();
+
+        //foreach (string s in surveytitle)
+
+        //    lbl_surveyid.Text += s.ToString();
+
+
+    }
+
+    protected void subBound(object sender, RepeaterItemEventArgs e)
+    {
+        SurveyClass objSurvey = new SurveyClass();
+        QuestionsClass objQuestion = new QuestionsClass();
+        // int surveyID = Convert.ToInt32(hdfSID.Value.ToString());
+
+        if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+        {
+            Label blwl = e.Item.FindControl("lbl_sID") as Label;
+            HiddenField hdfSID = e.Item.FindControl("hdf_SID") as HiddenField;
+            int surveyID = Convert.ToInt32(hdfSID.Value.ToString());
+            HiddenField hdfQID = e.Item.FindControl("hdf_QID") as HiddenField;
+            int questionID = Convert.ToInt32(hdfQID.Value.ToString());
+
+            // Repeater r2 = (Repeater)e.Item.FindControl("r2");
+            Repeater nestedRepeater = e.Item.FindControl("rpt_surveytitle") as Repeater;
+            nestedRepeater.DataSource = objSurvey.GetSurveyByID(surveyID);
+            nestedRepeater.DataBind();
+
+            Repeater nestedRepeater2 = e.Item.FindControl("rpt_question") as Repeater;
+            nestedRepeater2.DataSource = objQuestion.GetQuestionByID(questionID);
+            nestedRepeater2.DataBind();
+        }
+    }
+
+    public string getResults()
+    {
+        HiddenField hdf = (HiddenField)FindControl("hdfID");
+        int surveyresponseid = Convert.ToInt32(hdf);
+        HiddenField hdfSID = (HiddenField)FindControl("hdf_SID");
+        int surveyID = Convert.ToInt32(hdfSID);
+
+        List<string> surveytitle = (from q in dbDC.Questions
+                                    join sq in dbDC.SurveyQuestions on q.ID equals sq.QuestionID
+                                    join sr in dbDC.SurveyResponses on sq.SurveyID equals sr.SurveyID
+                                    join s in dbDC.Surveys on sr.SurveyID equals s.ID
+                                    where s.ID == surveyID
+                                    select s.Title).ToList();
+        Literal lbl_surveyid = (Literal)FindControl("lbl_surveyid");
+        foreach (string s in surveytitle)
+        {
+            lbl_surveyid.Text += s.ToString();
+        };
+        return lbl_surveyid.Text;
+
+
+    }
+
+    //SORTING FOR SURVEY TITLE
+
+    protected void titleDown_Click(object sender, EventArgs e)
+    {
+        sortDescendingTitle();
+    }
+
+    private void sortDescendingTitle()
+    {
+        using (dbDC)
+        {
+            var select = from survey in dbDC.SurveyResponses
+                         select survey;
+            ltv_results.DataSource = select.OrderByDescending(item => item.SurveyID);
+            ltv_results.DataBind();
+            lkb_titleDown.Visible = false;
+            lkb_titleUp.Visible = true;
+        }
+    }
+
+
+    private void sortAscendingTitle()
+    {
+        using (dbDC)
+        {
+            var select = from question in dbDC.SurveyResponses
+                         select question;
+            ltv_results.DataSource = select.OrderBy(item => item.SurveyID);
+            ltv_results.DataBind();
+            lkb_titleUp.Visible = false;
+            lkb_titleDown.Visible = true;
+        }
+    }
+    protected void titleUp_Click(object sender, EventArgs e)
+    {
+        sortAscendingTitle();
+    }
+
+    //end sorting for title
+
+
+    //SORTING FOR SurveyQuestion
+
+    protected void questionDown_Click(object sender, EventArgs e)
+    {
+        sortDescendingQuestion();
+    }
+
+    private void sortDescendingQuestion()
+    {
+        using (dbDC)
+        {
+            var select = from question in dbDC.SurveyResponses
+                         select question;
+            ltv_results.DataSource = select.OrderByDescending(item => item.QuestionID);
+            ltv_results.DataBind();
+            lkb_questionDown.Visible = false;
+            lkb_questionUp.Visible = true;
+        }
+    }
+
+
+    private void sortAscendingQuestion()
+    {
+        using (dbDC)
+        {
+            var select = from question in dbDC.SurveyResponses
+                         select question;
+            ltv_results.DataSource = select.OrderBy(item => item.QuestionID);
+            ltv_results.DataBind();
+            lkb_questionUp.Visible = false;
+            lkb_questionDown.Visible = true;
+        }
+    }
+    protected void questionUp_Click(object sender, EventArgs e)
+    {
+        sortAscendingQuestion();
+    }
+
+    //end sorting for question
+
+    //sorting for survey ID
+
+    protected void srDown_Click(object sender, EventArgs e)
+    {
+        sortDescendingSR();
+    }
+
+    private void sortDescendingSR()
+    {
+        using (dbDC)
+        {
+            var select = from sr in dbDC.SurveyResponses
+                         select sr;
+            ltv_results.DataSource = select.OrderByDescending(item => item.ID);
+            ltv_results.DataBind();
+            lkb_srDown.Visible = false;
+            lkb_srUp.Visible = true;
+        }
+    }
+
+
+    private void sortAscendingSR()
+    {
+        using (dbDC)
+        {
+            var select = from sr in dbDC.SurveyResponses
+                         select sr;
+            ltv_results.DataSource = select.OrderBy(item => item.ID);
+            ltv_results.DataBind();
+            lkb_srUp.Visible = false;
+            lkb_srDown.Visible = true;
+        }
+    }
+    protected void srUp_Click(object sender, EventArgs e)
+    {
+        sortAscendingSR();
+    }
+
+}
+
+

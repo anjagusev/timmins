c2023b1484d84f1209a74cb8d92d9a6cc531b6cc
Anja <anja.gusev@gmail.com>
2013-04-10 19:54:36 -0400

    Adding the survey to the master page.

--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -25,13 +25,15 @@ public partial class MasterPage : System.Web.UI.MasterPage
 
     //creating an instance of the article class
     articleClass objNews = new articleClass();
-
     protected void Page_Load(object sender, EventArgs e)
     {
+        getActive();
         if (!Page.IsPostBack)
         {
             _subRebind();
         }
+        this.MsgBoxContinue.Value = Page.ClientScript.GetPostBackEventReference(ShowSurvey, string.Empty);
+        PopulateSurvey();
     }
 
     public void subMobile(object sender, EventArgs e)
@@ -150,4 +152,249 @@ public partial class MasterPage : System.Web.UI.MasterPage
         Session["test"] = searchbox.Text;
         Response.Redirect("result.aspx");//sends the text to result.aspx and opens it
     }
+
+    /******BEGIN SURVEY FEEDBACK FEATURE******************/
+
+
+    //set the survey id that is active for the whole form
+    int surveyid;
+    // SurveysDataContext surveyDC = new SurveysDataContext();
+    SurveyResponse surveyresponse = new SurveyResponse();
+
+    private void getActive()
+    {
+        DataClassesDataContext dbDC = new DataClassesDataContext();
+        List<int> activesurvey = (from s in dbDC.Surveys
+                                  where s.Status == "Active"
+                                  select s.ID).ToList();
+
+        surveyid = activesurvey[0];
+    }
+
+
+
+    protected void ddlSurveys_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+
+    }
+
+
+
+    private void PopulateSurvey()
+    {
+        DataClassesDataContext dbDC = new DataClassesDataContext();
+
+        SurveyQuestion surveyquestions = new SurveyQuestion();
+        SurveyQuestionClass sQC = new SurveyQuestionClass();
+
+
+        SurveyResponseClass objSurveyResponse = new SurveyResponseClass();
+
+        btnSubmit.Enabled = true;
+
+
+        List<Question> questions = (from p in dbDC.Questions
+                                    join q in dbDC.GetTable<SurveyQuestion>() on p.ID equals q.QuestionID
+                                    where q.SurveyID == surveyid
+                                    select p).ToList();
+
+
+        Table tbl = new Table();
+        tbl.ID = "tbl_response";
+        tbl.Width = Unit.Percentage(100);
+        TableRow tr;
+        TableCell tc;
+        TextBox txt;
+        CheckBox cbk;
+        DropDownList ddl;
+        ListBox lb;
+        RequiredFieldValidator rfv;
+
+        foreach (Question q in questions)
+        {
+            tr = new TableRow();
+            tc = new TableCell();
+            tc.Width = Unit.Percentage(25);
+            tc.Text = q.Text;
+            tc.Attributes.Add("id", q.ID.ToString());
+            tr.Cells.Add(tc);
+            tc = new TableCell();
+
+            if (q.QuestionType.ToLower() == "singlelinetextbox")
+            {
+                txt = new TextBox();
+                txt.ID = "txt_" + q.ID;
+                txt.Width = Unit.Percentage(40);
+                tc.Controls.Add(txt);
+
+                rfv = new RequiredFieldValidator();
+                rfv.ID = "rfv_" + txt.ID;
+                rfv.ControlToValidate = txt.ID;
+                rfv.Text = "*Required";
+                tc.Controls.Add(rfv);
+
+            }
+
+            if (q.QuestionType.ToLower() == "multiline")
+            {
+                txt = new TextBox();
+                txt.ID = "txt_" + q.ID;
+                txt.TextMode = TextBoxMode.MultiLine;
+                txt.Width = Unit.Percentage(40);
+                tc.Controls.Add(txt);
+                rfv = new RequiredFieldValidator();
+                rfv.ID = "rfv_" + txt.ID;
+                rfv.ControlToValidate = txt.ID;
+                rfv.Text = "*Required";
+                tc.Controls.Add(rfv);
+            }
+
+            if (q.QuestionType.ToLower() == "yesorno")
+            {
+                cbk = new CheckBox();
+                cbk.ID = "cbk_" + q.ID;
+                cbk.AutoPostBack = false;
+                tc.Controls.Add(cbk);
+                rfv = new RequiredFieldValidator();
+                rfv.ID = "rfv_" + cbk.ID;
+                rfv.ControlToValidate = cbk.ID;
+                rfv.Text = "*Required";
+                tc.Controls.Add(rfv);
+            }
+
+            if (q.QuestionType.ToLower() == "singleselect")
+            {
+                ddl = new DropDownList();
+                ddl.ID = "ddl_" + q.ID;
+                ddl.Width = Unit.Percentage(41);
+                if (!string.IsNullOrEmpty(q.Options))
+                {
+                    string[] values = q.Options.Split(',');
+                    foreach (string v in values)
+                        ddl.Items.Add(v.Trim());
+                }
+                tc.Controls.Add(ddl);
+
+                rfv = new RequiredFieldValidator();
+                rfv.ID = "rfv_" + ddl.ID;
+                rfv.ControlToValidate = ddl.ID;
+                rfv.Text = "*Required";
+                tc.Controls.Add(rfv);
+            }
+
+            if (q.QuestionType.ToLower() == "multiselect")
+            {
+                lb = new ListBox();
+                lb.ID = "lb" + q.ID;
+                lb.Width = Unit.Percentage(41);
+                lb.SelectionMode = ListSelectionMode.Multiple;
+                // lb.SelectionMode = SelectionMode.MultiExtended;
+                if (!string.IsNullOrEmpty(q.Options))
+                {
+                    string[] values = q.Options.Split(',');
+                    foreach (string v in values)
+                        lb.Items.Add(v.Trim());
+                }
+                tc.Controls.Add(lb);
+
+                rfv = new RequiredFieldValidator();
+                rfv.ID = "rfv_" + lb.ID;
+                rfv.ControlToValidate = lb.ID;
+                rfv.Text = "*Required";
+                tc.Controls.Add(rfv);
+            }
+
+            tc.Width = Unit.Percentage(80);
+            tr.Cells.Add(tc);
+            tbl.Rows.Add(tr);
+        }
+        pnlSurvey.Controls.Add(tbl);
+        pnlSurvey.Visible = true;
+
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+
+        SurveyResponseClass objSurveyResponse = new SurveyResponseClass();
+
+
+        List<SurveyResponse> response = GetSurveyReponse().ToList();
+        foreach (SurveyResponse sres in response)
+        {
+            int _surveyid = sres.SurveyID;
+            int _questionid = sres.QuestionID;
+            string _value = sres.Value;
+            int _filledby = Convert.ToInt32(sres.FilledBy);
+
+            objSurveyResponse.commitInsert(_surveyid, _questionid, _value, _filledby);
+
+        }
+
+    }
+
+    private List<SurveyResponse> GetSurveyReponse()
+    {
+        int surveyID = 44;
+        List<SurveyResponse> response = new List<SurveyResponse>();
+
+        pnlSurvey.FindControl("tbl_response");
+
+        foreach (Control ctr in pnlSurvey.Controls)
+        {
+            if (ctr is Table)
+            {
+                Table tbl = ctr as Table;
+
+                foreach (TableRow tr in tbl.Rows)
+                {
+
+                    SurveyResponse sres = new SurveyResponse();
+                    sres.FilledBy = 2;
+                    sres.SurveyID = surveyID;
+                    sres.QuestionID = Convert.ToInt32(tr.Cells[0].Attributes["ID"]);
+                    TableCell tc = tr.Cells[1];
+                    foreach (Control ctrc in tc.Controls)
+                    {
+                        if (ctrc is TextBox)
+                        {
+                            sres.Value = (ctrc as TextBox).Text.ToString();
+                        }
+                        else if (ctrc is DropDownList)
+                        {
+                            sres.Value = (ctrc as DropDownList).SelectedValue;
+                        }
+
+                        else if (ctrc is CheckBox)
+                        {
+                            sres.Value = (ctrc as CheckBox).Checked.ToString();
+                        }
+
+
+                        else if (ctrc is ListBox)
+                        {
+                            sres.Value = "";
+                            foreach (ListItem item in (ctrc as ListBox).Items)
+                            {
+                                if (item.Selected)
+                                {
+                                    sres.Value += " " + item.Text;
+                                }
+                            }
+
+
+                        }
+
+                    }
+
+                    response.Add(sres);
+
+                }
+
+            }
+        }
+        return response.ToList();
+    }
+
 }

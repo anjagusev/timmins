f95a32c4b63f382bee8cd3573cfa7c8caf2ddff5
Anja <anja.gusev@gmail.com>
2013-04-10 21:07:37 -0400

    updating the jquery for the modal window

---
+++ js/survey.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $(function () {
+        $("#MsgBox").dialog({
+            autoOpen: false,
+            modal: true,
+            width: "auto",
+            height: "auto"
+        });
+    });
+});
+function showjQueryDialog() {
+    //check to see how many checkboxes there are and how many of those are checked.
+    $("#MsgBox").dialog("open");
+    $('#MsgBox').parent().appendTo($("form:first"));
+}
+function closejQueryDialog() {
+    $("#MsgBox").dialog("close");
+}
